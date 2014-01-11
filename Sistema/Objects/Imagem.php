29e0ea5a27a52d3cf9d6aa6f25a42b131ee233c8
Rafael Duarte Gervásio da Silva <rafael.dgs.1993@gmail.com>
2014-01-11 15:27:24 -0200

    Pasta Objects
    Pasta com os Objetos usados para transição de dados

---
+++ Sistema/Objects/Imagem.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Description of Imagem
+ *
+ * @author Rafael
+ */
+namespace Data\Object;
+class Imagem {
+    public $id; //Int
+    public $caminhoImagem; //MediumBlob
+    public $link; //String
+    public $idTipoImagem; //Int
+    public $idPostagem; //Int
+}
+
+?>
---
+++ Sistema/Objects/Tag.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Description of Tag
+ *
+ * @author Rafael
+ */
+namespace Data\Object;
+class Tag {
+    public $id; //Int
+    public $tag; //String
+
+}
+
+?>
